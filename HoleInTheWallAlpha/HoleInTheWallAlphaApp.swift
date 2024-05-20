ed6fe83fd6549b77a3d7b849462419eeb9845f7e
Ariq Syahalam <70618885+ariqsyahalam@users.noreply.github.com>
2024-05-20 11:57:16 +0700

    Initial Commit

---
+++ HoleInTheWallAlpha/HoleInTheWallAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoleInTheWallAlphaApp.swift
+//  HoleInTheWallAlpha
+//
+//  Created by Reyhan Ariq Syahalam on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HoleInTheWallAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
